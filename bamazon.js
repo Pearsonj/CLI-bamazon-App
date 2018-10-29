4a3a07c9781020f191fb693bb9c0daa5cd4ab54c
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-10-29 11:04:40 -0700

    Got all functions working!

--- bamazon.js
+++ bamazon.js
@@ -6,54 +6,239 @@ var connection = mysql.createConnection({
 
     ports: 3306,
 
-    user:"root",
+    user: "root",
 
     password: "4321Mommy",
     database: "bamazon_db"
 });
 
-function searchArtist() {
-    inquirer.prompt([{
-        type: "input",
-        message: "who are you looking for?",
-        name: "artist"
-    }]).then(function (itemid) {
 
-        var artist = itemid.artist;
-        var sql = "SELECT * FROM products WHERE id = ?";
-        connection.query(sql, [artist], function (err, res) {
+
+// function chooseCat() {
+//     inquirer.prompt([{
+//         type: "list",
+//         choices: ["Tech", "Random", "Automotive", "Kitchen", "Furniture"],
+//         message: "What Are You Interested In?",
+//         name: "cat"
+//     }]).then(function (catSelect) {
+//         if (catSelect.cat === "Tech") {
+//             function listItems() {
+
+//                 connection.query("SELECT * FROM products WHERE department_name = 'Tech'", function (err, res) {
+//                     if (err) throw err;
+//                    ;
+//                     for (var i = 0; i < res.length; i++) {
+//                         console.log('-------------------------------');
+//                         console.log("ID: " + res[i].id);
+//                         console.log('Item: ' + res[i].product_name);
+//                         console.log('Category: ' + res[i].department_name);
+//                         console.log('Price: ' + res[i].price);
+//                         console.log('Quantity: ' + res[i].quantity);
+//                         console.log('-------------------------------')
+//                     }
+//                     
+//                 });
+//             }
+//             listItems();
+//         }
+
+
+//         if (catSelect.cat === "Random") {
+//             function listItems() {
+
+//                 connection.query("SELECT * FROM products WHERE department_name = 'Random'", function (err, res) {
+//                     if (err) throw err;
+//                    ;
+//                     for (var i = 0; i < res.length; i++) {
+//                         console.log('-------------------------------');
+//                         console.log("ID: " + res[i].id);
+//                         console.log('Item: ' + res[i].product_name);
+//                         console.log('Category: ' + res[i].department_name);
+//                         console.log('Price: ' + res[i].price);
+//                         console.log('Quantity: ' + res[i].quantity);
+//                         console.log('-------------------------------')
+//                     }
+//                     connection.end();
+//                 });
+//             }
+//             listItems();
+//         }
+
+//         if (catSelect.cat === "Automotive") {
+//             function listItems() {
+
+//                 connection.query("SELECT * FROM products WHERE department_name = 'Automotive'", function (err, res) {
+//                     if (err) throw err;
+//                    ;
+//                     for (var i = 0; i < res.length; i++) {
+//                         console.log('-------------------------------');
+//                         console.log("ID: " + res[i].id);
+//                         console.log('Item: ' + res[i].product_name);
+//                         console.log('Category: ' + res[i].department_name);
+//                         console.log('Price: ' + res[i].price);
+//                         console.log('Quantity: ' + res[i].quantity);
+//                         console.log('-------------------------------')
+//                     }
+//                     connection.end();
+//                 });
+//             }
+//             listItems();
+//         }
+
+//         if (catSelect.cat === "Kitchen") {
+//             function listItems() {
+
+//                 connection.query("SELECT * FROM products WHERE department_name = 'Kitchen'", function (err, res) {
+//                     if (err) throw err;
+//                    ;
+//                     for (var i = 0; i < res.length; i++) {
+//                         console.log('-------------------------------');
+//                         console.log("ID: " + res[i].id);
+//                         console.log('Item: ' + res[i].product_name);
+//                         console.log('Category: ' + res[i].department_name);
+//                         console.log('Price: ' + res[i].price);
+//                         console.log('Quantity: ' + res[i].quantity);
+//                         console.log('-------------------------------')
+//                     }
+//                     connection.end();
+//                 });
+//             }
+//             listItems();
+//         }
+
+//         if (catSelect.cat === "Furniture") {
+//             function listItems() {
+
+//                 connection.query("SELECT * FROM products WHERE department_name = 'Furniture'", function (err, res) {
+//                     if (err) throw err;
+//                    ;
+//                     for (var i = 0; i < res.length; i++) {
+//                         console.log('-------------------------------');
+//                         console.log("ID: " + res[i].id);
+//                         console.log('Item: ' + res[i].product_name);
+//                         console.log('Category: ' + res[i].department_name);
+//                         console.log('Price: ' + res[i].price);
+//                         console.log('Current Quantity: ' + res[i].quantity);
+//                         console.log('-------------------------------')
+//                     }
+//                     connection.end();
+//                 });
+//             }
+
+//             listItems();
+//             setTimeout(buyItem(), 10000);
+
+//         } 
+//     });
+// }
+// chooseCat();
+
+// function buyItem() {
+
+//     inquirer.prompt([{
+//             type: "input",
+//             message: "Please Input The ID Number Of The Item You Would Like To Purchase",
+//             name: "itemId"
+//         },
+//         {
+//             type: "input",
+//             message: "How Many Would You Like?",
+//             name: "quantity"
+//         }
+//     ]).then(function (purchase) {
+//         var sql = "UPDATE products SET ? WHERE ?",
+//             query = connection.query(
+//                 sql,
+//                 [{
+//                         quantity: quantity - purchase.quantity
+//                     },
+//                     {
+
+//                         id: purchase.itemId
+//                     }
+//                 ],
+//                 console.log(query.sql)
+//             )
+
+//     })
+// }
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    console.log("connected " + connection.threadId);
+
+    function listItems() {
+
+        connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err;
-            console.log("connected as id " + connection.threadId);
             for (var i = 0; i < res.length; i++) {
                 console.log('-------------------------------');
-                        console.log("ID: " + res[i].id);
-                        console.log('Item: ' + res[i].product_name);
-                        console.log('Category: ' + res[i].department_name);
-                        console.log('Price: ' + res[i].price);
-                        console.log('Quantity: ' + res[i].quantity);
-                        console.log('-------------------------------')
+                console.log("ID: " + res[i].id);
+                console.log('Item: ' + res[i].product_name);
+                console.log('Category: ' + res[i].department_name);
+                console.log('Price: ' + res[i].price);
+                console.log('Quantity: ' + res[i].quantity);
+                console.log('-------------------------------')
             }
-            connection.end();
-        })
-    })
-}
 
-function listItems(){
+        });
 
-connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    for (var i = 0; i < res.length; i++) {
-        console.log('-------------------------------');
-        console.log("ID: " + res[i].id);
-        console.log('Item: ' + res[i].product_name);
-        console.log('Category: ' + res[i].department_name);
-        console.log('Starting Bid: ' + res[i].price);
-        console.log('Current Bid: ' + res[i].quantity);
-        console.log('-------------------------------')
+
     }
+
+    listItems();
+
+    setTimeout(function buyItem() {
+        inquirer.prompt([{
+                type: "input",
+                message: "Please Input The ID Number Of The Item You Would Like To Purchase",
+                name: "itemId"
+            },
+            {
+                type: "input",
+                message: "How Many Would You Like?",
+                name: "quantity"
+            }
+        ]).then(function (purchase) {
+            var query1 = "SELECT quantity FROM products WHERE ?";
+            var purchased = purchase.quantity;
+            var quantity = connection.query(query1, {id:purchase.itemId},function (err, res) {
+                if(err) throw err;
+                var quantityInt = res[0].quantity;
+                if(purchased <= quantityInt){
+                var total = quantityInt - purchased;
+                console.log(quantityInt);
+                console.log(purchased);
+                console.log(total);
+                
+                var sql = "UPDATE products SET ? WHERE ?";
+                var query = connection.query(sql,
+
+                [{
+                        quantity: total
+                    },
+
+                    {
+                        id: purchase.itemId
+                    }
+                ],
+
+            )
+
+            console.log('updated');
+            connection.end();
+            
+                }
+                if(purchased > quantityInt){
+                    console.log("We Only Have " + quantityInt + " Left!")
+                    connection.end();
+                }
+
+               
+            })   
+        })
+
+    }, 1000)
 });
-
-}
-
-
