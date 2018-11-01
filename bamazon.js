427089a5388cc1bd096b89e61f5ad9b3b7953e06
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-10-31 18:07:30 -0700

    added some code comments

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+// My connection to MySQL database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
     password: "4321Mommy",
     database: "bamazon_db"
 });
-
+//This is where I call MySQL database to send me back a list of all my items
 connection.connect(function (err) {
     if (err) throw err;
 
@@ -21,6 +21,7 @@ connection.connect(function (err) {
 
         connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err;
+            //Runnning a for loop to console log my items
             for (var i = 0; i < res.length; i++) {
                 console.log('-------------------------------');
                 console.log("ID: " + res[i].id);
@@ -32,10 +33,11 @@ connection.connect(function (err) {
             }
         });
     }
-
+    //Calling my list function
     listItems();
-
+    // setting a small time out to make sure my next function does not interfere with my first function
     setTimeout(function buyItem() {
+        //inquirer funciton to ask what the customer wants to purchase
         inquirer.prompt([{
                 type: "input",
                 message: "Please Input The ID Number Of The Item You Would Like To Purchase",
@@ -47,6 +49,7 @@ connection.connect(function (err) {
                 name: "quantity"
             }
         ]).then(function (purchase) {
+            //MySQL function to call that calls for the item the customer defines in the previous inquirer question
             var query1 = "SELECT quantity,product_name,price FROM products WHERE ?";
             var purchased = purchase.quantity;
             var quantity = connection.query(query1, {
@@ -61,12 +64,9 @@ connection.connect(function (err) {
                     console.log("\n");
                     console.log("Final Checkout Total: " + itemPrice * purchased + " Dollars");
                     console.log("------------------------");
-                    // console.log(quantityInt);
-                    // console.log(purchased);
-                    // console.log(total);
                     console.log("Enjoy Your Brand New " + productName + "!")
                     console.log("------------------------");
-                    
+                    //this is where we un the update query to update the quantity of the item that was just bought
                     var sql = "UPDATE products SET ? WHERE ?";
                     var query = connection.query(sql,
                         [{
@@ -82,6 +82,7 @@ connection.connect(function (err) {
                     console.log("\n");
                     connection.end();
                 }
+                //If there is not enough of something this if statement is ran.
                 if (purchased > quantityInt) {
                     console.log("We Only Have " + quantityInt + " Left!")
                     console.log("------------------------\n");
